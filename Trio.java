152fc4f70c3a7471607222dee5bd275e2589b5b9
gHUME15 <Grayson.hume@stu.daviess.kyschools.us>
2025-04-07 14:29:41 +0000

    Done and did not change after you went over the answers

--- Trio.java
+++ Trio.java
@@ -1,1 +1,25 @@
 //Write your answer here
+public class Trio {
+    private Sandwich sandwich;
+    private Salad Salad;
+    private Drink drink;
+
+    public Trio(Sandwitch sandwich,Salad salad,Drink drink) {
+        Sandwich = sandwich;
+        Salad = salad;
+        Drink = drink;
+    }
+
+    public double getPrice(){
+        double priceSand = sandwich.getPrice();
+        double priceSald = salad.getPrice();
+        double priceDrin = drink.getPrice();
+    }
+
+
+    Public String getName() {
+        return sandwich.getname()+ "/" + salad.getname()+ "/" + drink.getname()+ "/" + "Trio"; 
+    }
+
+
+}
